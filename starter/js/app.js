031d323c88ae7606863c4588f18fe1cf6db133a5
lazertester <austin.techhead@gmail.com>
2015-09-29 00:53:21 -0600

    Cleaned up formatting before sleep.

--- starter/js/app.js
+++ starter/js/app.js
@@ -9,25 +9,29 @@ const StartWars = React.createClass({
 
     render() {
         return (<ShipList source={this.props.source}/>
-    )
-  }
+        )
+    }
 })
 
 //Should contain and render a list of ships.
 var ShipList = React.createClass({
     getInitialState(){
-        return{
+        return {
             page: 1,
             ships: []
         }
     },
 
     previousPage(){
-        this.setState(function(previousState, currentProps){ return{page: previousState.page - 1}});
+        this.setState(function (previousState, currentProps) {
+            return {page: previousState.page - 1}
+        });
     },
 
     nextPage(){
-        this.setState(function(previousState, currentProps){ return{page: previousState.page + 1}});
+        this.setState(function (previousState, currentProps) {
+            return {page: previousState.page + 1}
+        });
     },
 
     componentDidMount() {
@@ -35,13 +39,13 @@ var ShipList = React.createClass({
         self.ships = [];
         return axios
             .get(this.props.source + 'starships/')
-            .then(response => self.setState({ships:response.data.results}))
+            .then(response => self.setState({ships: response.data.results}))
     },
 
     renderShips() {
-        return _.map(this.state.ships, function(ship) {
-                    return <Ship key={ship.name} ship={ship} />;
-                });
+        return _.map(this.state.ships, function (ship) {
+            return <Ship key={ship.name} ship={ship}/>;
+        });
     },
 
     render(){
@@ -51,7 +55,8 @@ var ShipList = React.createClass({
             </ul>
             <button onClick={this.previousPage}>Previous Page</button>
             <span>Page {this.state.page}</span>
-            <button onClick={this.nextPage}>NextPage</button></div>;
+            <button onClick={this.nextPage}>NextPage</button>
+        </div>;
     }
 })
 
@@ -73,20 +78,24 @@ var Ship = React.createClass({
 
     render() {
         var cost = Number(this.props.ship.cost_in_credits).toLocaleString();
-        return <div><li className="list-group-item"><div><span onClick={this.openModal}>{this.props.ship.name}, {cost} Credits</span></div></li>
+        return <div>
+            <li className="list-group-item">
+                <div><span onClick={this.openModal}>{this.props.ship.name}, {cost} Credits</span></div>
+            </li>
             <Modal
                 isOpen={this.state.modalIsOpen}
                 onRequestClose={this.closeModal}>
 
                 <h2>{this.props.ship.name}</h2>
+
                 <div>{cost} Credits</div>
                 <button onClick={this.closeModal}>close</button>
             </Modal>
         </div>
     },
-
-
 });
 
-React.render(<div><div>Testing</div><StartWars source="http://swapi.co/api/"/></div>,
+React.render(<div>
+        <div>Testing</div>
+        <StartWars source="http://swapi.co/api/"/></div>,
     document.querySelector('.app'))
